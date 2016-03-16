0c8fd46a6c1fb0efdda8e72b8bcd55f31ee4aaaf
andresrabinovich <andresrabinovich@gmail.com>
2016-03-16 18:27:09 -0300

    agrega un mindmap de la presentacion y empieza con el theme warsaw

---
+++ notas/Presentación.mm
@@ -0,0 +1,87 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Presentaci&#xf3;n" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1458159587356"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<node TEXT="Introducci&#xf3;n" POSITION="right" ID="ID_90868772" CREATED="1458158947065" MODIFIED="1458159784839" VSHIFT="-20">
+<edge COLOR="#0000ff"/>
+<node TEXT="Introducci&#xf3;n Biol&#xf3;gica" ID="ID_1811407543" CREATED="1458153824177" MODIFIED="1458159784837"/>
+<node TEXT="Motivaciones" ID="ID_280182642" CREATED="1458152294626" MODIFIED="1458158962561" VSHIFT="-10"/>
+<node TEXT="Cambios transcripcionales en respuesta a estr&#xe9;s abi&#xf3;tico en A.thaliana" ID="ID_945725125" CREATED="1458152795745" MODIFIED="1458161977936"/>
+<node TEXT="El problema del agrupamiento" ID="ID_1372257621" CREATED="1458152827689" MODIFIED="1458162175515">
+<node TEXT="Medidas de similaridad" ID="ID_765834024" CREATED="1458159001577" MODIFIED="1458159035825" VSHIFT="10"/>
+<node TEXT="Tipos de agrupamientos" ID="ID_1651908880" CREATED="1458159044353" MODIFIED="1458159067905"/>
+</node>
+</node>
+<node TEXT="An&#xe1;lisis de relevamientos transcripcionales" POSITION="right" ID="ID_1954327334" CREATED="1458159454625" MODIFIED="1458162420913">
+<edge COLOR="#ff00ff"/>
+<node TEXT="M&#xe9;todos km, ds1, ds4" ID="ID_857322742" CREATED="1458159465009" MODIFIED="1458162420912" VSHIFT="60"/>
+<node TEXT="Caracterizaci&#xf3;n de grupos km, ds1, ds4" ID="ID_1755895524" CREATED="1458159523673" MODIFIED="1458159576832" VSHIFT="-20"/>
+<node TEXT="El problema de la escala" ID="ID_843746895" CREATED="1458153785161" MODIFIED="1458162192665" VSHIFT="-50"/>
+</node>
+<node TEXT="Congruencia biol&#xf3;gica" POSITION="right" ID="ID_647993460" CREATED="1458153799105" MODIFIED="1458159587355" HGAP="60" VSHIFT="-30">
+<edge COLOR="#00ff2d"/>
+<node TEXT="Ontolog&#xed;a g&#xe9;nica (GO)" ID="ID_704732305" CREATED="1458154606308" MODIFIED="1458162808641"/>
+<node TEXT="Interacting densities" ID="ID_1096833908" CREATED="1458153978617" MODIFIED="1458162827864"/>
+<node TEXT="BHI bajo?" ID="ID_394341648" CREATED="1458154002225" MODIFIED="1458159624291">
+<node TEXT="Coherencia entre m&#xe9;trica transcripcional y espacio GO (KTA global)" ID="ID_1861470562" CREATED="1458155113921" MODIFIED="1458155551870"/>
+<node TEXT="KTA local para&#xa;Modulaci&#xf3;n de heterogeneidades&#xa;transcripcionales" ID="ID_309606378" CREATED="1458155187273" MODIFIED="1458159624288" VSHIFT="50">
+<node TEXT="M&#xe9;trica mixta" ID="ID_1834689038" CREATED="1458155297113" MODIFIED="1458155693973"/>
+<node TEXT="M&#xe9;todo heur&#xed;stico" ID="ID_1543759087" CREATED="1458155335265" MODIFIED="1458155348055"/>
+<node TEXT="Interpretaci&#xf3;n biol&#xf3;gica" ID="ID_208716350" CREATED="1458155348657" MODIFIED="1458155353391"/>
+</node>
+</node>
+</node>
+<node TEXT="Conclusiones y perspectivas" POSITION="right" ID="ID_1863270821" CREATED="1458155359793" MODIFIED="1458159442264" HGAP="50" VSHIFT="20">
+<edge COLOR="#ff0000"/>
+</node>
+</node>
+</map>
